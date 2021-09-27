bc7078081cfbe553dca2f358671e3cd1a76f51c5
Andersen <andersen5447@gmail.com>
2021-09-27 14:24:41 +0900

    start working on matrix hw

---
+++ src/MatrixMultiply/MatrixMultiply/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixMultiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
