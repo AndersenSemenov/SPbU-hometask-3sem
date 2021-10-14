9045e876ab7def11e2dc27936528097771e542bf
Andersen <andersen5447@gmail.com>
2021-10-14 23:15:03 +0300

    created solution structure

---
+++ Task_02/Lazy/Lazy/OneThreadLazy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lazy
+{
+    public class OneThreadLazy<T>: ILazy<T>
+    {
+        public T value { get; private set; }
+        private bool IsCalculated = false;
+        private Func<T> supplier;
+
+        public OneThreadLazy(Func<T> supplier)
+            => this.supplier = supplier ?? throw new ArgumentNullException("supplier is null");
+
+        public T Get()
+        {
+            if (!IsCalculated)
+            {
+                IsCalculated = true;
+                value = supplier();
+                supplier = null;
+            }
+            
+            return value;
+        }
+    }
+}
